c2063abffe7fcd95dd8f3d1d39c8e4fbef082197
Joraaver Chahal <machspeeds@gmail.com>
2014-08-22 10:48:25 -0700

    DONE: Second plot

--- plot2.R
+++ plot2.R
@@ -5,10 +5,9 @@
 #########################################
 
 # Question
-# Have total emissions from PM2.5 decreased in the United States from 
-# 1999 to 2008? Using the base plotting system, make a plot showing the total 
-# PM2.5 emission from all sources for each of the years 
-# 1999, 2002, 2005, and 2008.
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (
+# fips == "24510") from 1999 to 2008? Use the base plotting system to make a 
+# plot answering this question.
 
 
 # Necessary Lines to read files
@@ -20,12 +19,12 @@ SCC <- readRDS("Source_Classification_Code.rds")
 library(tidyr)
 library(dplyr)
 
-# Plan of attack: First, I'll give the NEI$year factor levels so that
-# I can make a data frame that is grouped by the year. Then, I should
-# be able to summarize over the sum of emissions (all are of the same
-# pollutant type) to get the emmissions for each year.
+# Plan of attack: This is similar to the first question. First, I'll filter
+# the NEI data frame, looking for the specified fips. Then, Ill group by the 
+# years, and summarize them.
 
-emission_sums <- select(NEI,Emissions,year) %>%
+emission_sums <- filter(NEI,fips=="24510") %>%
+    select(Emissions,year) %>%
     group_by(year) %>%
     summarize(emissions = sum(Emissions))
 emission_sums <- as.data.frame(emission_sums)
@@ -34,7 +33,7 @@ emission_sums <- as.data.frame(emission_sums)
 
 plot(emission_sums$year,emission_sums$emissions/1000000,type='o',
      xaxt = 'n',pch=21,bg='grey',lwd=2,xlab="Year",ylab="PM 2.5 Emissions (in millions of tons)",
-     main= 'PM 2.5 Emission Levels in the United States')
+     main= 'PM 2.5 Emission Levels in Baltimore City, Maryland')
 axis(1,at =emission_sums$year,labels=emission_sums$year)
-dev.copy(png, file="plot1.png")
+dev.copy(png, file="plot2.png")
 dev.off()
